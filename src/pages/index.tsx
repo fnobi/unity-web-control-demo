b915b8557acf77472730d7b5776848630198da29
Fujisawa Shin <fujisawa@akiba.kayac.studio>
2022-03-12 21:56:34 +0900

    unity実装を２種きりかえる

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import { css } from "@emotion/react";
 import { percent } from "~/lib/cssUtil";
+import { useState } from "react";
 import { BASE_PATH } from "~/local/constants";
 import UnityEmbed from "~/components/UnityEmbed";
 
@@ -16,13 +17,46 @@ const wrapperStyle = css({
   height: percent(100)
 });
 
-const PageIndex: NextPage = () => (
-  <div css={wrapperStyle}>
-    <UnityEmbed
-      buildName="karting-microgame"
-      unityBuildRoot={`${BASE_PATH}unity-webgl`}
-    />
-  </div>
-);
+const PageIndex: NextPage = () => {
+  const [game, setGame] = useState<1 | 2>(1);
+  return (
+    <div css={wrapperStyle}>
+      <div>
+        {game === 1 ? (
+          <UnityEmbed
+            buildName="karting-microgame"
+            unityBuildRoot={`${BASE_PATH}unity-webgl`}
+          />
+        ) : null}
+        {game === 2 ? (
+          <UnityEmbed
+            buildName="demo"
+            unityBuildRoot={`${BASE_PATH}demo/Build`}
+          />
+        ) : null}
+        <p>
+          <label>
+            <input
+              type="radio"
+              checked={game === 1}
+              onChange={() => setGame(1)}
+            />
+            &nbsp;karting microgame
+          </label>
+        </p>
+        <p>
+          <label>
+            <input
+              type="radio"
+              checked={game === 2}
+              onChange={() => setGame(2)}
+            />
+            &nbsp;demo
+          </label>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default PageIndex;
