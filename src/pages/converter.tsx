c3a50c903a2b45258529c5cbb8f6ab19cab3a074
Fujisawa Shin <fujisawa@akiba.kayac.studio>
2022-04-27 19:24:34 +0900

    threeとunityで同じ空間をつくる

--- src/pages/converter.tsx
+++ src/pages/converter.tsx
@@ -1,9 +1,33 @@
 import { NextPage } from "next";
+import {
+  BoxGeometry,
+  Euler,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  Vector3,
+  WebGLRenderer
+} from "three";
 import { css } from "@emotion/react";
 import { em, percent } from "~/lib/cssUtil";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { BASE_PATH } from "~/local/constants";
 import UnityEmbed2021 from "~/components/UnityEmbed2021";
+
+// このへんはunity側と前提を揃える必要があるところ
+const WIDTH = 960;
+const HEIGHT = 600;
+
+const CAMERA_FOV = 60;
+const CAMERA_CLIPPING_NEAR = 0.3;
+const CAMERA_CLIPPING_FAR = 1000;
+const CAMERA_POSITION = new Vector3(0, 1, -10);
+const CAMERA_ROTATION = new Euler(0, Math.PI, 0);
+
+const BOX_SIZE_X = 2;
+const BOX_SIZE_Y = 1;
+const BOX_SIZE_Z = 3;
 
 const wrapperStyle = css({
   position: "fixed",
@@ -30,6 +54,44 @@ const threeCanvasStyle = css({
 const PageConverter: NextPage = () => {
   const threeCanvasRef = useRef<HTMLCanvasElement>(null);
 
+  useEffect(() => {
+    const { current: canvas } = threeCanvasRef;
+    if (!canvas) {
+      return () => {};
+    }
+
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(
+      CAMERA_FOV,
+      WIDTH / HEIGHT,
+      CAMERA_CLIPPING_NEAR,
+      CAMERA_CLIPPING_FAR
+    );
+
+    const renderer = new WebGLRenderer({ canvas });
+
+    const geometry = new BoxGeometry(BOX_SIZE_X, BOX_SIZE_Y, BOX_SIZE_Z);
+    const material = new MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.copy(CAMERA_POSITION);
+    camera.rotation.copy(CAMERA_ROTATION);
+
+    let t = -1;
+
+    function animate() {
+      t = window.requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    }
+
+    animate();
+
+    return () => {
+      window.cancelAnimationFrame(t);
+    };
+  }, []);
+
   return (
     <div css={wrapperStyle}>
       <div css={columnStyle}>
@@ -38,8 +100,8 @@ const PageConverter: NextPage = () => {
           unityBuildRoot={`${BASE_PATH}/unity-webgl/sample2021/Build`}
         />
         <canvas
-          width={960}
-          height={600}
+          width={WIDTH}
+          height={HEIGHT}
           ref={threeCanvasRef}
           css={threeCanvasStyle}
         />
