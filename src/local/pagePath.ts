f1b875a39298f08a2315d6d2b20a7490b432a1e6
Fujisawa Shin <fujisawa@akiba.kayac.studio>
2022-11-24 13:39:34 +0900

    掃除

--- src/local/pagePath.ts
+++ src/local/pagePath.ts
@@ -1,6 +1,5 @@
 import PageEntry from "~/lib/PageEntry";
 import { BASE_URL } from "~/local/constants";
 
+// eslint-disable-next-line import/prefer-default-export
 export const PAGE_TOP = new PageEntry(BASE_URL);
-export const PAGE_ABOUT = PAGE_TOP.child("about");
-export const PAGE_ARTICLE = PAGE_TOP.child("article");
